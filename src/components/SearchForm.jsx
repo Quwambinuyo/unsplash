64e64d461a90b4793d5a50bb0956286e18561d43
Quwam Binuyo <qbinuyo7@gmail.com>
2025-04-14 09:28:09 +0100

    implement search form functionality and add dark theme support in CSS

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useGlobalContext } from "../context/context";
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.elements);
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default SearchForm;

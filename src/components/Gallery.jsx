3cf343ec70782ad20ac83e5c31e6ba3f9167ff26
Quwam Binuyo <qbinuyo7@gmail.com>
2025-04-14 10:29:08 +0100

    implement image fetching functionality in Gallery component using React Query

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,21 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=WvXsev_jeSuqrVGunnTdFT_7j4UgSsvrxAx0e3sNu70&query=cat";
 
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+
+  console.log(response);
   return <h2>Gallery</h2>;
 };
 

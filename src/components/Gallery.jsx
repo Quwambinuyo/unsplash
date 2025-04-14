2cbafa85e23fefb9b3f14dbb303dcbfced944c7f
Quwam Binuyo <qbinuyo7@gmail.com>
2025-04-14 11:27:43 +0100

    fix: update API URL to use environment variable for client ID

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useGlobalContext } from "../context/context";
 
-const url = `https://api.unsplash.com/search/photos?client_id=WvXsev_jeSuqrVGunnTdFT_7j4UgSsvrxAx0e3sNu70`;
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
